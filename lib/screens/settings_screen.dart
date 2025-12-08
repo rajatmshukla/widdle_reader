4cb6cd505aa2ce71ba140221175c641c6aedc7af
Rajat Shukla <rajatmshukla@gmail.com>
2025-12-07 23:38:36 -0500

    v1.2

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -809,7 +809,7 @@ class _SettingsScreenState extends ConsumerState<SettingsScreen> with SingleTick
 
             // Version
             Text(
-              'Version 1.1.0',
+              'Version 1.2',
               style: textTheme.bodyMedium?.copyWith(
                 color: colorScheme.onSurfaceVariant,
               ),
